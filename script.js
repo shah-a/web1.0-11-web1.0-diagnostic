5aa636ac405aa42021bb970a22778d4d87531642
Ali Shah <s.ashah@outlook.com>
2020-10-06 16:26:35 -0400

    Wrote JS function skeletons

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const widthDisplay = document.querySelector("width");
 const heightDisplay = document.querySelector("height");
 const colorDisplay = document.querySelector("color");
 const boxDisplay = document.querySelector("color-box");
+
+function updateValue(property) {
+
+}
+
+function updateBoxSize(width, height) {
+
+}
+
+function updateBoxColor(color) {
+
+}
+
+widthDisplay.addEventListener("change", () => {
+  updateValue();
+  updateBoxSize();
+})
+
+heightDisplay.addEventListener("change", () => {
+  updateValue();
+  updateBoxSize();
+})
+
+colorDisplay.addEventListener("change", updateBoxColor);
