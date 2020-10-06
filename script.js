aac00cee5800cb8e73f6debad1c57e489329418a
Ali Shah <s.ashah@outlook.com>
2020-10-06 16:14:55 -0400

    HTML and CSS layout starting to come together, about to start JS

---
+++ script.js
@@ -0,0 +1,4 @@
+const widthDisplay = document.querySelector("width");
+const heightDisplay = document.querySelector("height");
+const colorDisplay = document.querySelector("color");
+const boxDisplay = document.querySelector("color-box");
